7970f9197de53afbcfc21ed1a30babaeb9fa2f73
Robert Sanins <rob.sanins@lovehoney.co.uk>
2021-02-18 15:19:34 +0000

    facebook integration and env set up

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  quizApplication
+//
+//  Created by Rob Sanins on 17/02/2021.
+//
+
+import Foundation
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import {GoogleSignin} from '@react-native-community/google-signin';
+import React, {useEffect} from 'react';
+import Config from 'react-native-config';
 import {AuthProvider} from './AuthProvider';
 import {Routes} from './Routes';
 
 export const Providers = () => {
+  useEffect(() => {
+    // initialize the Google SDK
+    GoogleSignin.configure({
+      webClientId: Config.WEB_CLIENT_ID,
+      iosClientId: Config.IOS_CLIENT_ID,
+    });
+  }, []);
+
   return (
     <AuthProvider>
       <Routes />
